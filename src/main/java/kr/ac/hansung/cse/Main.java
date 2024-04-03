7ed5012a1f033eadd5585e14c6a0d98450b5bc82
Namyun Kim <nykim@hansung.ac.kr>
2024-04-03 16:45:01 +0900

    modify some variable names

--- src/main/java/kr/ac/hansung/cse/Main.java
+++ src/main/java/kr/ac/hansung/cse/Main.java
@@ -5,46 +5,48 @@ import kr.ac.hansung.cse.dao.InstructorDao;
 import kr.ac.hansung.cse.dao.StudentDao;
 import kr.ac.hansung.cse.entity.Course;
 import kr.ac.hansung.cse.entity.Instructor;
+import kr.ac.hansung.cse.entity.InstructorDetail;
 import kr.ac.hansung.cse.entity.Student;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Arrays;
+import java.util.List;
 
-// One-to-Many Unidirectional
+// One-to-Many Uni-directional
 //public class Main {
 //    public static void main(String[] args) {
 //        ClassPathXmlApplicationContext context =
 //                new ClassPathXmlApplicationContext("applicationContext.xml");
 //
-//        InstructorDao InstructorDao = context.getBean(InstructorDao.class);
-//        CourseDao CourseDao = context.getBean(CourseDao.class);
+//        InstructorDao instructorDao = context.getBean(InstructorDao.class);
+//        CourseDao courseDao = context.getBean(CourseDao.class);
 //
-//        Instructor Instructor1 = new Instructor("Namyun Kim", "nykim@hansung.ac.kr");
-//        Course Course1 = new Course("웹프레임워크");
-//        Course Course2 = new Course("오픈소스소프트웨어");
+//        Instructor instructor1 = new Instructor("Namyun Kim", "nykim@hansung.ac.kr");
+//        Course course1 = new Course("웹프레임워크");
+//        Course course2 = new Course("오픈소스소프트웨어");
 //
-//        Instructor Instructor2 = new Instructor("Jaemon Lee", "jmlee@hansung.ac.kr");
-//        Course Course3 = new Course("iOS 프로그래밍");
-//        Course Course4 = new Course("안드로이드 프로그래밍");
+//        Instructor instructor2 = new Instructor("Jaemon Lee", "jmlee@hansung.ac.kr");
+//        Course course3 = new Course("iOS 프로그래밍");
+//        Course course4 = new Course("안드로이드 프로그래밍");
 //
-//        InstructorDao.save(Instructor1);
-//        InstructorDao.save(Instructor2);
+//        instructorDao.save(instructor1);
+//        instructorDao.save(instructor2);
 //
 //        // Instructor 객체를 먼저 저장한 후, Course 객체를 저장해야 합니다.
 //        // Course 객체 내부에 Instructor 참조가 있기 때문에, Instructor가 먼저 영속화되어야 합니다.
-//        Course1.setInstructor(Instructor1);
-//        Course2.setInstructor(Instructor1);
-//        Course3.setInstructor(Instructor2);
-//        Course4.setInstructor(Instructor2);
+//        course1.setInstructor(instructor1);
+//        course2.setInstructor(instructor1);
+//        course3.setInstructor(instructor2);
+//        course4.setInstructor(instructor2);
 //
-//        CourseDao.save(Course1);
-//        CourseDao.save(Course2);
-//        CourseDao.save(Course3);
-//        CourseDao.save(Course4);
+//        courseDao.save(course1);
+//        courseDao.save(course2);
+//        courseDao.save(course3);
+//        courseDao.save(course4);
 //
 //        // 저장된 데이터 조회
-//        List<Instructor> instructors = InstructorDao.findAll();
-//        List<Course> Courses = CourseDao.findAll();
+//        List<Instructor> instructors = instructorDao.findAll();
+//        List<Course> courses = courseDao.findAll();
 //
 //        System.out.println("Instructors:");
 //        for (Instructor Instructor : instructors) {
@@ -52,14 +54,14 @@ import java.util.Arrays;
 //        }
 //
 //        System.out.println("\nCourses:");
-//        for (Course Course : Courses) {
-//            System.out.println( "Instructor: " + Course.getInstructor().getFullName() +
-//                    ", Course: " + Course.getTitle());
+//        for (Course course : courses) {
+//            System.out.println( "Instructor: " + course.getInstructor().getFullName() +
+//                    ", Course: " + course.getTitle());
 //        }
 //    }
 //}
 
-// One-to-Many birectional
+// One-to-Many bi-directional
 
 //public class Main {
 //    public static void main(String[] args) {
@@ -91,26 +93,26 @@ import java.util.Arrays;
 //}
 
 
-// One-to-One unirectional
+ //One-to-One uni-directional
 //public class Main {
 //    public static void main(String[] args) {
 //        ClassPathXmlApplicationContext context =
 //                new ClassPathXmlApplicationContext("applicationContext.xml");
 //
-//        InstructorDao InstructorDao = context.getBean(InstructorDao.class);
+//        InstructorDao instructorDao = context.getBean(InstructorDao.class);
 //
 //        // InstructorDetail 객체 생성
 //        InstructorDetail detail =
-//                new InstructorDetail("youtube.com/TheJavaChannel", "Coding");
+//                new InstructorDetail("youtube.com/@HansungUvi", "교육");
 //
 //        // Instructor 객체 생성 및 InstructorDetail 설정
 //        Instructor instructor = new Instructor("Namyun Kim", "nykim@hansung.ac.kr");
 //        instructor.setInstructorDetail(detail);
 //
-//        InstructorDao.save(instructor);
+//        instructorDao.save(instructor);
 //
 //        // 저장된 객체 조회
-//        Instructor storedInstructor = InstructorDao.findById( instructor.getId());
+//        Instructor storedInstructor = instructorDao.findById( instructor.getId());
 //        System.out.println("Retrieved Instructor: " + storedInstructor.getFullName());
 //
 //        // 연결된 InstructorDetail 정보 출력
